f0b250486a3b96c7f27182831126c53e67e3c80a
Mayank Maroti <116047644+May5599@users.noreply.github.com>
2024-12-02 22:53:42 +0000

    converstion of data types

---
+++ 01_Basic/03_coverstionOperation.js
@@ -0,0 +1,22 @@
+let score = "abs"
+
+console.log(typeof score);
+
+let valInNumber = Number(score)
+console.log(typeof valInNumber);
+console.log(valInNumber);
+
+// "33" => 33
+//"33abc" => NaN
+// true => 1; fasle => 0;
+
+let isLoggedIn = 1
+
+let bolleanIsloggedIn = Boolean(isLoggedIn)
+
+console.log(bolleanIsloggedIn);
+
+
+// 1=> true; 0=> false
+//"" => false
+// hitesh => true
