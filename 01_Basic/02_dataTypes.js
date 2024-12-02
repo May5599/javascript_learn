753c034ea3e06f920bdad25ee37d625a590ffea8
Mayank Maroti <116047644+May5599@users.noreply.github.com>
2024-12-02 22:38:32 +0000

    datatypes learn

---
+++ 01_Basic/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use Strict"; // treat all js code as new version 
+
+//alert(3+3) // we are using node.js not browser 
+
+let name = "mayank"
+let age  = 25
+let isLoggedIn = false
+
+
+// numebr ==> 2 to power 53
+// bigint
+// string => " "
+// bollean => true/fasle
+// null => standalone value
+// undefiend => 
+// symbol => unique 
+
+
+
+// object 
+
+console.log(typeof "mayank");
+
