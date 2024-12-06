73985fd9b5cb4ebb3fb43fad9aaa95dea19fcadf
Mayank Maroti <116047644+May5599@users.noreply.github.com>
2024-12-06 18:35:00 +0000

    operators learning

--- 01_Basic/03_coverstionOperation.js
+++ 01_Basic/03_coverstionOperation.js
@@ -1,10 +1,10 @@
 let score = "abs"
 
-console.log(typeof score);
+// console.log(typeof score);
 
 let valInNumber = Number(score)
-console.log(typeof valInNumber);
-console.log(valInNumber);
+// console.log(typeof valInNumber);
+// console.log(valInNumber);
 
 // "33" => 33
 //"33abc" => NaN
@@ -14,9 +14,34 @@ let isLoggedIn = 1
 
 let bolleanIsloggedIn = Boolean(isLoggedIn)
 
-console.log(bolleanIsloggedIn);
+// console.log(bolleanIsloggedIn);
 
 
 // 1=> true; 0=> false
 //"" => false
 // hitesh => true
+
+
+//  Operations *******************
+let value = 3
+let negValue = - value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2)
+// console.log(2*2);
+// console.log(2/3);
+
+
+let str1 = "hello"
+let str2 = " mayank"
+
+let str3 = str1 +str2
+// console.log(str3);
+
+// console.log("1"+2+2); 
+
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
