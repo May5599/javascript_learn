2c650511984bc09bbeddb6cd90a8e457e785aaef
Mayank Maroti <116047644+May5599@users.noreply.github.com>
2024-12-07 00:44:58 +0000

    comp learn

---
+++ 01_Basic/datatypes-summary.js
@@ -0,0 +1,27 @@
+//  Primitive |
+
+//  7 types : String, Number , Bollean , null, undefiend , Symbol, BigInt
+
+const score = 100
+
+const id = Symbol('123')
+const anotherid = Symbol ('123')
+
+//console.log(id == anotherid);
+
+
+
+
+// Reference type (Non primitive): Array, Objects, Functions
+
+const heros = ["shkatiman", "naagraj", "doga"]
+
+let myObj = {
+    name: "mayank",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+    
+}
